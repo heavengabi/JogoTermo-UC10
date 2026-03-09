cd6b9f8e57b6df953b3f011093a81f5331af0adf
Miguel Vargas Ferreira <miguelferreira8967@gmail.com>
2026-03-09 17:47:55 -0300

    refactor(iniciarJogo.js): Melhora como as palavras aparecem no terminal

--- iniciarJogo.js
+++ iniciarJogo.js
@@ -6,15 +6,17 @@ const rl = require('readline-sync');
 
 function iniciarJogo() {
 
-    console.log("Iniciando o jogo")
-
-    const palavraSecreta = sortearPalavra()
-    let tentativas = 0 // isso aqui é pra iniciar o jogo com 0 tentativas
-    let acertou = false
+  
+    console.clear();
+    console.log("Jogo iniciado!");
+    const palavraSecreta = sortearPalavra();
+    let tentativas = 0; // isso aqui é pra iniciar o jogo com 0 tentativas
+    let acertou = false;
     const historico = [] //esse array ele guarda todoas as tentativas do jogador, por isso começa vazio
     while (tentativas < 6 && !acertou) {
+        
         console.log(`Tentativa: ${tentativas + 1} /6`) // aqui ele mostra o numero de tentativas 
-        let palpite = rl.question("Digite uma palavra.").toLowerCase() // pra digitar a palavra e se escrever em capslock ela vira caixa baixa😺
+        let palpite = rl.question("\nDigite uma palavra:\n").toLowerCase() // pra digitar a palavra e se escrever em capslock ela vira caixa baixa😺
         if (!validarPalpite(palpite)) 
         {
             console.log("Palpite inválido. Tente novamente.")
@@ -22,28 +24,29 @@ function iniciarJogo() {
 
         }
 
-        const resultado = compararPalavras(palpite, palavraSecreta)
-        historico.push({ palpite, resultado })  // .push guarda palpite e o resultado
-        console.log("/n Seus chutes: ")
+        const resultado = compararPalavras(palpite, palavraSecreta);
+        historico.push({ palpite, resultado });  // .push guarda palpite e o resultado
+        console.log("\n Seus chutes: ");
         historico.forEach((item, i) => {
-            console.log(`${i + 1}. ${item.palpite} ${item.resultado}`) //pega as palavras digitadas e o resultado kk
+            console.log(`${i + 1}. ${item.palpite} ${item.resultado}`); //pega as palavras digitadas e o resultado kk
         })
 
 
         if (palpite === palavraSecreta) {
-            acertou = true // aqui muda o status do cara, la no começo tava falso ai acertou e ficou true 😺
-            console.log(`Parabéns!! você acertou a palavra secreta: ${palavraSecreta}`)
-            console.log("Fim do jogo!")
+            acertou = true ;// aqui muda o status do cara, la no começo tava falso ai acertou e ficou true 😺
+            console.log(`Parabéns!! você acertou a palavra secreta: ${palavraSecreta}`);
+            console.log("Fim do jogo!");
 
         }
         console.log("")
         tentativas++
-    }
+         }
 
-    if (!acertou) 
-    {
-        console.log(`Suas tentativas acabaram. A palvra era ${palavraSecreta}`)
-    }
+        if (!acertou) 
+        {
+         console.log(`Suas tentativas acabaram. A palvra era ${palavraSecreta}`)
+        }
+        
 
 
 }
